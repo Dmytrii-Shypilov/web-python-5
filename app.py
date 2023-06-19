437ec358236405f26bd023321ed2eafe1c8506b9
Dmytrii Shypilov <95936002+Dmytrii-Shypilov@users.noreply.github.com>
2023-06-19 16:45:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,60 @@
 import asyncio
 from aiohttp import ClientSession
-from datetime import datetime
-import requests
-
-def get_url(period=None):
-    if not period:
-        period = str(datetime.now().date()).split("-")
-        period.reverse()
-    api_date_format = (".").join(period)
-    base_url = f'https://api.privatbank.ua/p24api/exchange_rates?date={api_date_format}'
-    return base_url
-
-def get_currency_info(data, currency=None):
-    pass
+from datetime import datetime, timedelta
+import pprint 
 
 
-async def fetch_currencies(session, period=None):
-    url = get_url(period)
-    async with session.get(url) as response:
-        result = await response.text()
-        return result
-        
+def get_currency_info(data, currency):
+    for day in data:
+        currency_data = list(filter(lambda x: x['currency'] == currency, day['exchangeRate']))[0]
+        print(f"Date: {day['date']}, Sale: {currency_data['saleRate']} UAH, Purchase: {currency_data['purchaseRate']} UAH")
 
+
+def format_date(date):
+    formatted_date = str(date.date()).split("-")
+    formatted_date.reverse()
+    result = (".").join(formatted_date)
+    return result
+
+
+def get_dates_list(period):
+    period = int(period)
+    dates_list = []
+    today = datetime.now()
+    dates_list.append(format_date(today))
+    if period > 0 and period <= 10:
+        for day in range(1, period):
+            date = format_date(today - timedelta(day))
+            dates_list.append(date)
+    return dates_list
+
+
+async def fetch_currencies(session, dates):
+    data = []
+    for date in dates:
+        url = f'https://api.privatbank.ua/p24api/exchange_rates?json&date={date}'
+        async with session.get(url) as response:
+            result = await response.json()
+            data.append(result)
+    return data
 
 def parse_instructions(command):
     args = command.split(" ")
-    while len(args) < 3:
-        args.append(None)
     return args
 
 
 async def main():
     async with ClientSession() as session:
         instructions = input("Enter your command >>> ")
-        command, date, currency = parse_instructions(instructions) 
-        data = await fetch_currencies(session, date)
-        get_currency_info(data, currency)
-        
+        command, currency, period = parse_instructions(instructions)
+        if command == "exchange":
+            dates = get_dates_list(period)
+            print("...Fetching data")
+            data = await fetch_currencies(session, dates)
+            get_currency_info(data, currency)
+       
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+   
